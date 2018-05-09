3651a3d5361264826d0bad375d8f2d3a4d144095
Ruedi <ruedi.luethi@mitlinks.ch>
2018-05-09 09:50:45 +0200

    mehr rechnung

--- Code/Diskretes_Modell.m
+++ Code/Diskretes_Modell.m
@@ -1,25 +1,32 @@
 clear;
 close all;
 
+% Behälter 1: Golf von Mexiko
+% Behälter 2: Nordmeer
+% --> Wasser gefriert zu Eis, Salz wird ausgelöst, höherer Salzgehalt > höhere Dichte
+% wenn aber Sstar_02 > Sstar_01, dann Sstar_01 - Sstar_02 < 0
+% und damit beta < 0 ...?
+% sollten aber alpha und beta nicht jeweils > 0 sein?
+
 % Anfangsbedingungen
-Tstar_01 = 1;
-Tstar_02 = 1.1;
-Tstar_0  = Tstar_01 - Tstar_02;
-Sstar_01 = 1.1;
-Sstar_02 = 1;
+Tstar_01 = 1;   % wärmer
+Tstar_02 = 0.65; % kälter
+Tstar_0  = Tstar_01 - Tstar_02; 
+Sstar_01 = 1;
+Sstar_02 = 0.5;
 Sstar_0  = Sstar_01 - Sstar_02;
 
 % Austauschraten
 k_T = 1;
-k_S = 1;
+k_S = 0.1; % damit gamma = 0.1 = k_S/k_T
 
 % weitere Proportionalitätskonstanten
-a = 1;
-b = 1;
-c = 1;
+a = 1; % > 0
+b = 1; % > 0
+c = 1; % > 0
 
 % dimensionslose Konstanten
-alpha = 2*(a*b/k_T)*Tstar_0;
+alpha = 2*(a*b/k_T)*Tstar_0; 
 beta  = 2*(a*c/k_T)*Sstar_0;
 gamma = k_S/k_T;
 
@@ -36,7 +43,7 @@ Sstar   = [Sstar_0]; % Differenz
 S       = [Sstar(end)/Sstar_0]; % dimensionslose Differenz
 
 % Zeitschritt dt^*
-dtstar = .1;
+dtstar = 0.1;
 dt     = k_T*dtstar; % dimensionslos
 
 % Begin der Simulation
@@ -50,8 +57,8 @@ for i = [1:amount_of_iterations/dtstar]
     dTstar_1 = ( k_T*(Tstar_01 - Tstar_1(end)) + abs(qstar)*(Tstar_2(end) - Tstar_1(end)) )*dtstar;
     dTstar_2 = ( k_T*(Tstar_02 - Tstar_2(end)) + abs(qstar)*(Tstar_1(end) - Tstar_2(end)) )*dtstar;
     
-    dSstar_1 = ( k_T*(Sstar_01 - Sstar_1(end)) + abs(qstar)*(Sstar_2(end) - Sstar_1(end)) )*dtstar;
-    dSstar_2 = ( k_T*(Sstar_02 - Sstar_2(end)) + abs(qstar)*(Sstar_1(end) - Sstar_2(end)) )*dtstar;
+    dSstar_1 = ( k_S*(Sstar_01 - Sstar_1(end)) + abs(qstar)*(Sstar_2(end) - Sstar_1(end)) )*dtstar;
+    dSstar_2 = ( k_S*(Sstar_02 - Sstar_2(end)) + abs(qstar)*(Sstar_1(end) - Sstar_2(end)) )*dtstar;
     
     tstar(end+1) = tstar(end) + dtstar;
     
@@ -69,37 +76,49 @@ for i = [1:amount_of_iterations/dtstar]
     Sstar(end+1) = Sstar(end) + dSstar;
     
     % dimensionslose Rechnung
-    q  = alpha*T(end) - beta*T(end);
+    q  = alpha*T(end) - beta*S(end);
     dT = (       (1 - T(end)) - abs(q)*T(end) )*dt;
     dS = ( gamma*(1 - S(end)) - abs(q)*S(end) )*dt;
     
     t(end+1) = t(end) + dt;
     T(end+1) = T(end) + dT;
-    S(end+1) = S(end) + dT;
+    S(end+1) = S(end) + dS;
 end
 
 row_count = 3;
+cols_count = 3;
 
-subplot(row_count,2,1);
+subplot(row_count,cols_count,1);
 plot(tstar, Tstar_1, tstar, Tstar_2);
 title('Temperatur');
+legend('T^*_1','T^*_2');
 
-subplot(row_count,2,2);
+subplot(row_count,cols_count,4);
+plot(tstar, Tstar, 'k', tstar, Tstar_1-Tstar_2, ':r');
+legend('T^*','T^*_1-T^*_2');
+
+subplot(row_count,cols_count,7);
+plot(t, T, 'k', k_T*tstar, Tstar./Tstar_0, ':r');
+legend('T','T^*/T^*_0');
+
+subplot(row_count,cols_count,2);
 plot(tstar, Sstar_1, tstar, Sstar_2);
 title('Salzgehalt');
+legend('S^*_1','S^*_2');
 
-subplot(row_count,2,3);
-plot(tstar, Tstar, 'k', tstar, Tstar_1-Tstar_2, ':w');
+subplot(row_count,cols_count,5);
+plot(tstar, Sstar, 'k', tstar, Sstar_1-Sstar_2, ':r');
+legend('S^*','S^*_1-S^*_2');
 
-subplot(row_count,2,4);
-plot(tstar, Sstar, 'k', tstar, Sstar_1-Sstar_2, ':w');
+subplot(row_count,cols_count,8);
+plot(t, S, 'k', k_T*tstar, Sstar./Sstar_0, ':r');
+legend('S','S^*/S^*_0');
 
-subplot(row_count,2,5);
-plot(t, T, 'k', k_T*tstar, Tstar./Tstar_0, ':w');
+% Gleichgewichtsuntersuchung
+g = @(q) alpha.*(1./(1+abs(q))) - beta.*(gamma./(gamma+abs(q)));
+q = linspace(-2,2,1000);
 
-subplot(row_count,2,6);
-plot(t, S, 'k', k_T*tstar, Sstar./Sstar_0, ':w');
-
-
-
-
+subplot(row_count,cols_count,3);
+plot(q,g(q),q,q,'k',[min(q),max(q)],[0,0],':k');
+axis([min(q) max(q) min(g(q))*2 max(g(q))*2]);
+title({['alpha=',num2str(alpha),', beta=',num2str(beta),', gamma=',num2str(gamma)],['g(0)=alpha-beta=',num2str(alpha-beta),' > 0']});
--- Code/skizze_fixpunkte.m
+++ Code/skizze_fixpunkte.m
@@ -1,15 +1,23 @@
 clear;
 close all;
 
+a = 1;
+b = 1;
+c = 1;
+
+
+
 hold on;
-for a = [1:0.1:3]
-    alpha = 1.5;
+variation_list = linspace(0.2,2,10);
+for variation = variation_list
+    alpha = variation;
     beta = 1;
-    gamma = 1/a;
+    gamma = 0.1;
 
     g = @(q) alpha.*(1./(1+abs(q))) - beta.*(gamma./(gamma+abs(q)));
 
     q = linspace(-2,2,1000);
 
-    plot(q,g(q));
+    plot(q,g(q),'DisplayName',[num2str(variation),'a-b=',num2str(alpha-beta)]);
 end
+legend('show');
